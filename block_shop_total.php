a5b18ac6cd6c747f3e7a7a3d6f1ab118b1f219ea
valery <valery.fremaux@gmail.com>
2016-09-26 12:02:36 +0200

    automated sync from SVN

--- block_shop_total.php
+++ block_shop_total.php
@@ -14,14 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * @package    block_shop_total
  * @category   blocks
- * @copyright  2013 Valery Fremaux
+ * @copyright  2016 Valery Fremaux
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
 
 require_once $CFG->dirroot.'/local/shop/lib.php';
 require_once $CFG->dirroot.'/local/shop/locallib.php';
@@ -33,36 +33,36 @@ use local_shop\Catalog;
 
 class block_shop_total extends block_base {
 
-    function init() {
+    public function init() {
         $this->title = get_string('blockname', 'block_shop_total');
     }
 
-    function applicable_formats() {
+    public function applicable_formats() {
         return array('all' => true, 'my' => true, 'course' => true);
     }
 
-    function specialization() {
+    public function specialization() {
         return false;
     }
 
-    function instance_allow_multiple() {
+    public function instance_allow_multiple() {
         return true;
     }
 
-    function get_content() {
-        global $USER, $CFG, $DB, $COURSE, $OUTPUT, $SESSION;
+    public function get_content() {
+        global $USER, $DB, $COURSE, $OUTPUT, $SESSION;
 
         if ($this->content !== null) {
             return $this->content;
         }
 
-        list($theShop, $theCatalog, $theBlock) = shop_build_context();
+        list($theshop, $thecatalog, $theblock) = shop_build_context();
         $view = optional_param('view', 'shop', PARAM_ALPHA);
 
         $this->content = new stdClass;
 
-        $categories = $theCatalog->get_categories();
-        $shopproducts = $theCatalog->get_all_products($categories);
+        $categories = $thecatalog->get_categories();
+        $shopproducts = $thecatalog->get_all_products($categories);
         $units = 0;
 
         if (isset($SESSION->shoppingcart->order)) {
@@ -72,7 +72,7 @@ class block_shop_total extends block_base {
         }
 
         $renderer = shop_get_renderer('front');
-        $renderer->load_context($theShop, $theCatalog, $theBlock);
+        $renderer->load_context($theshop, $thecatalog, $theblock);
 
         // Order total block.
 
@@ -80,14 +80,14 @@ class block_shop_total extends block_base {
         $this->content->text .= $renderer->order_totals();
         $this->content->text .= '</div>';
 
-        // Order item counting block 
+        // Order item counting block.
 
         $this->content->text .= $OUTPUT->heading(get_string('order', 'block_shop_total'));
         $this->content->text .= '<div id="order-detail">';
         $this->content->text .= $renderer->order_detail($shopproducts);
         $this->content->text .= '</div>';
 
-        // Print navigation controller in right column
+        // Print navigation controller in right column.
 
         $nextstyle = ($units) ? 'opacity:1.0' : 'opacity:0.5';
         $nextdisabled = ($units) ? '' : 'disabled="disabled"';
@@ -98,28 +98,33 @@ class block_shop_total extends block_base {
             $this->content->text .= '<p align="center"><center>';
             $this->content->text .= '<form name="driverform" action="'.$shopurl.'">';
             $this->content->text .= '<input type="hidden" name="view" value="shop" />';
-            $this->content->text .= '<input type="hidden" name="shopid" value="'.$theShop->id.'" />';
-            if (!empty($theBlock)) {
-                $this->content->text .= '<input type="hidden" name="blockid" value="'.$theBlock->instance->id.'" />';
+            $this->content->text .= '<input type="hidden" name="shopid" value="'.$theshop->id.'" />';
+            if (!empty($theblock)) {
+                $this->content->text .= '<input type="hidden" name="blockid" value="'.$theblock->instance->id.'" />';
             }
             $this->content->text .= '<input type="hidden" name="what" value="navigate" />';
-            $this->content->text .= '<input type="submit" id="next-button" title="'.$overtext.'" name="go" value="'.get_string('next', 'block_shop_total').'" onclic="check_empty_order()" '.$nextdisabled.' style="'.$nextstyle.'" />';
-            $this->content->text .= '<input type="button" name="clearall" value="'.get_string('reset', 'block_shop_total').'" onclick="this.form.what.value=\'clearall\';this.form.submit();" />';
+            $this->content->text .= '<input type="submit"
+                                            id="next-button"
+                                            title="'.$overtext.'"
+                                            name="go" value="'.get_string('next', 'block_shop_total').'"
+                                            onclic="check_empty_order()" '.$nextdisabled.' style="'.$nextstyle.'" />';
+            $this->content->text .= '<input type="button"
+                                            name="clearall"
+                                            value="'.get_string('reset', 'block_shop_total').'"
+                                            onclick="this.form.what.value=\'clearall\';this.form.submit();" />';
             $this->content->text .= '</form>';
             $this->content->text .= '</center></p>';
         }
 
         $this->content->footer = '';
 
-        unset($filteropt); // memory footprint
-
         return $this->content;
     }
 
-    /*
-     * Hide the title bar when none set..
+    /**
+     * Hide the title bar when none set.
      */
-    function hide_header() {
+    public function hide_header() {
         return empty($this->config->title);
     }
 }
--- version.php
+++ version.php
@@ -15,21 +15,23 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details
+ * Version details.
  *
- * @package    block_shop_total
- * @category   blocks
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package     block_shop_total
+ * @category    blocks
+ * @author      Valery Fremaux <valery.fremaux@gmail.com>
+ * @copyright   2016 onwards Valery Fremaux (http://www.mylearningfactory.com)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014021900;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014050800;        // Requires this Moodle version
-$plugin->component = 'block_shop_total'; // Full name of the plugin (used for diagnostics)
-$plugin->maturity = MATURITY_BETA; // Full name of the plugin (used for diagnostics)
-$plugin->release = '2.7.0 (Build 2014021900)'; // Full name of the plugin (used for diagnostics)
+$plugin->version   = 2016022500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2014050800;        // Requires this Moodle version.
+$plugin->component = 'block_shop_total'; // Full name of the plugin (used for diagnostics).
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '2.7.0 (Build 2016022500)';
+$plugin->dependencies = array('local_shop' => '*');
 
 // Non moodle attributes.
 $plugin->codeincrement = '2.7.0000';
