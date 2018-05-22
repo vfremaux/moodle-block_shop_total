c347b0f1117c6b6e16d4eadd3ca9be9a4eb6f94d
valery <valery.fremaux@gmail.com>
2018-05-22 13:58:53 +0200

    Pushing new M35 versions

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016022500;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2017110800;        // Requires this Moodle version.
+$plugin->requires  = 2018042700;        // Requires this Moodle version.
 $plugin->component = 'block_shop_total'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.4.0 (Build 2016022500)';
+$plugin->release = '3.5.0 (Build 2016022500)';
 $plugin->dependencies = array('local_shop' => '*');
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.4.0000';
+$plugin->codeincrement = '3.5.0000';
