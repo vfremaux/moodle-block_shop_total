3f8430632ff6fc3478b279f9b97d6a90cf52e1b9
Valery Fremaux <valery.fremaux@gmail.com>
2022-11-21 21:29:42 +0100

    %1

--- version.php
+++ version.php
@@ -27,12 +27,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016022500;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2020061500;        // Requires this Moodle version.
+$plugin->requires  = 2022041900;        // Requires this Moodle version.
 $plugin->component = 'block_shop_total'; // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_STABLE;
-$plugin->supported = [39,311];
-$plugin->release = '3.9.0 (Build 2016022500)';
-$plugin->dependencies = array('local_shop' => 2022090900);
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '4.0.0 (Build 2016022500)';
+$plugin->supported = [40, 40];
+$plugin->dependencies = array('local_shop' => '*');
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.9.0002';
+$plugin->codeincrement = '4.0.0002';
